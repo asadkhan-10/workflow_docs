50faa2c48ebb8f9a012730c5c31cece1169d8605
Asad ali khan <asad.khan.connect1@gmail.com>
2026-04-08 16:43:20 +0500

    added alembic migrations

--- app/models.py
+++ app/models.py
@@ -1,15 +1,17 @@
 # app/models.py
+from pydantic import constr
 from sqlalchemy import Column, Integer, String, Text, TIMESTAMP, ForeignKey
 from sqlalchemy.sql.expression import text
 from .database import Base
+
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                       server_default=text("now()"))
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
+
 
 class Workflow(Base):
     __tablename__ = "workflows"
@@ -18,7 +20,7 @@ class Workflow(Base):
     raw_input = Column(Text, nullable=False)  # plain English or JSON
     input_type = Column(String, nullable=False)  # "text" or "json"
     documentation = Column(Text)  # AI generated output
-    owner_id = Column(Integer, ForeignKey("users.id",
-                      ondelete="CASCADE"), nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                       server_default=text("now()"))
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
