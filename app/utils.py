084445b1ebe29103f8540c4ed3ec65563f9a7432
Asad ali khan <asad.khan.connect1@gmail.com>
2026-04-11 12:28:57 +0500

    added bcrypt version pin

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
